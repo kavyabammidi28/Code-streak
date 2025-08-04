9f9fb88cfea39855c4b0811ccb785587f0120f96
Kavya kumari Bammidi <kavyabammidi28@gmail.com>
2025-08-04 17:03:30 +0530

    Add files via upload

---
+++ DAY4 Prime Number.py
@@ -0,0 +1,13 @@
+# ✅ Day 04: Prime number
+# Problem: Write a function that checks if a given number is a prime number.
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(7))  # True
+print(is_prime(10)) # False

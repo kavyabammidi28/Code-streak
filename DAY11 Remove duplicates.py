791e2759ce8035b319f08c2765d4613e39bc0ce8
Kavya Bammidi <kavyabammidi28@gmail.com>
2025-08-12 18:32:00 +0530

    Add files via upload

---
+++ DAY11 Remove duplicates.py
@@ -0,0 +1,8 @@
+def remove_duplicates(lst):
+    result = []
+    for item in lst:
+        if item not in result:
+            result.append(item)
+    return result
+
+print(remove_duplicates([1, 2, 2, 3, 4, 3, 5]))

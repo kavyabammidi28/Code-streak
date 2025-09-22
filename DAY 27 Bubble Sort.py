fba860e2ab03350edf8db06b520560f8e54e065e
Kavya Bammidi <kavyabammidi28@gmail.com>
2025-09-22 16:46:04 +0530

    Add files via upload

---
+++ DAY 27 Bubble Sort.py
@@ -0,0 +1,20 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # swap
+    return arr
+
+# Example usage
+print(bubble_sort([64, 25, 12, 22, 11]))
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # swap
+    return arr
+
+# Example usage
+print(bubble_sort([64, 25, 12, 22, 11]))
---
+++ DAY 28 WORD COUNT.py
@@ -0,0 +1,7 @@
+def count_words(sentence):
+    words = sentence.split()
+    return len(words)
+
+# Example usage
+print(count_words("Python is fun and powerful"))  # 5
+print(count_words("Keep pushing your coding streak"))  # 5
